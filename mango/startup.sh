a7c3fd2d46e4e85224bd2fb83338473c1cac9204
TrulleN <bkushtrullen@gmail.com>
2025-11-02 12:53:18 +0100

    Add files via upload

--- mango/startup.sh
+++ mango/startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash/
 
-wlr-randr --output DVI-D-1 --on --pos 0,0 &
-wlr-randr --output DP-1 --on --pos 1280,0 &
-wlr-randr --output HDMI-A-1 --on --pos 3200,0 &
+wlr-randr --output DVI-D-1 --on --pos 0,0 
+wlr-randr --output DP-1 --on --pos 1280,0 
+wlr-randr --output HDMI-A-1 --on --pos 3200,0 
 wlr-randr --output DP-1 --mode 1920x1080@143.996002  
-
+swaybg -i /home/truls/wallpapers/trulsskidorhoger.png --mode fill
+waybar
+kitty
