0cc44e51063533a60f1913ca5c688645941e9c6a
TrulleN <bkushtrullen@gmail.com>
2025-11-02 12:52:53 +0100

    Add files via upload

---
+++ mango/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+wlr-randr --output DVI-D-1 --on --pos 0,0 &
+wlr-randr --output DP-1 --on --pos 1280,0 &
+wlr-randr --output HDMI-A-1 --on --pos 3200,0 &
+wlr-randr --output DP-1 --mode 1920x1080@143.996002  
+
